root-project/root

Mark deserialized inline functions used so the DeclUnloader keeps them.

interpreter/llvm/src/tools/clang/lib/CodeGen/CodeGenModule.cpp
@@ -788,6 +788,14 @@ void CodeGenModule::SetCommonAttributes(const Decl *D,
 
   if (D->hasAttr<UsedAttr>())
     addUsedGlobal(GV);
+  else if (const FunctionDecl* FD = dyn_cast<FunctionDecl>(D)) {
+     if (FD->isFromASTFile() && GV->hasLinkOnceODRLinkage()) {
+        // An inline function.
+        // Mark them used such that the DeclReverter does not
+        // unload it.
+        addUsedGlobal(GV);
+     }
+  }
 }
 
 void CodeGenModule::setNonAliasAttributes(const Decl *D,

yundhe/cling-clang-study

lib/CodeGen/CodeGenModule.cpp
@@ -952,6 +952,14 @@ void CodeGenModule::SetCommonAttributes(const Decl *D,
 
   if (D && D->hasAttr<UsedAttr>())
     addUsedGlobal(GV);
+  else if (const FunctionDecl* FD = dyn_cast_or_null<FunctionDecl>(D)) {
+     if (FD->isFromASTFile() && GV->hasLinkOnceODRLinkage()) {
+        // An inline function.
+        // Mark them used such that the DeclReverter does not
+        // unload it.
+        addUsedGlobal(GV);
+     }
+  }
 }
 
 void CodeGenModule::setAliasAttributes(const Decl *D,